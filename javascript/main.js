494f08e11293d755c0401a4f8f030ec088280fa4
Owl <owl.official.mailbox@protonmail.com>
2022-07-13 02:33:55 +0300

    Fix

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,10 @@
 $(document).ready(function () {
-<<<<<<< HEAD
     $(".slider__content").slick({
         arrows: true,
         infinite: true,
         slidesToShow: 1,
         prevArrow: $('.slick__prev'),
         nextArrow: $('.slick__next'),
-=======
-    $(".single-item").slick({
-        arrows: true,
-        infinite: true,
-        slidesToShow: 1,
->>>>>>> 3917b54fbbc3cfbf2965149644b352073c0fc7c5
     });
 });
 
