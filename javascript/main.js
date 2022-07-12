e167bff4aad92fc90aa90979f144212b31a3a95b
Bear <danekgdgdg@gmail.com>
2022-07-13 02:16:36 +0300

    [Carousel] Added

--- javascript/main.js
+++ javascript/main.js
@@ -5,3 +5,12 @@ $(document).ready(function () {
         slidesToShow: 1,
     });
 });
+
+// $(document).ready(function () {
+//     $(".product__box").slick({
+//         arrows: true,
+//         infinite: true,
+//         slidesToShow: 4,
+//         slidesToSscroll: 4,
+//     });
+// });
