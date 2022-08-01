83d5f0f03254227d7714fb574bde49b53edad665
Bear <danekgdgdg@gmail.com>
2022-08-01 13:58:25 +0300

    [Fixed] Catalog button

--- javascript/main.js
+++ javascript/main.js
@@ -110,16 +110,16 @@ $(document).ready(function () {
             },
         ],
     });
-}); 
-
+});
 
 // Catalog Menu
-function openNav() {
-    document.getElementById("myNav").style.display = "block";
-}
-
-function closeNav() {
-    document.getElementById("myNav").style.display = "none";
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 }
 
 //  Modal window
